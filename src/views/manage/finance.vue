c4ec417d6320884b82ab22965d733c49648ea3b1
hiwangling <4676572@qq.com>
2019-08-13 15:30:03 +0800

    1

--- src/views/manage/finance.vue
+++ src/views/manage/finance.vue
@@ -1074,7 +1074,7 @@ export default {
           oid: this.oid,
           totalprice: this.totalprice,
           operator: this.info.realname,
-          record_sign: this.record_sign
+          record_sign: this.record_sign_info
         }
         financePay(data)
           .then(res => {
